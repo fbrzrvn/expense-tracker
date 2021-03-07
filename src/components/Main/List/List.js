336930586b2089850e362b7de65cea490f7bdfaf
fabrizio <faber.rvn@gmail.com>
2021-03-07 09:19:14 +0100

    #5 List

---
+++ src/components/Main/List/List.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  List as MUIList,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  ListItemSecondaryAction,
+  IconButton,
+  Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
+
+import useStyles from './styles';
+
+const List = () => {
+
+  const classes = useStyles();
+
+  const transactions = [
+    {id: 1, type: "Income", category: "business", amount: 50, date: new Date().toLocaleString()},
+    {id: 2, type: "Expense", category: "pets", amount: 50, date: new Date().toLocaleString()},
+    {id: 3, type: "Income", category: "salaries", amount: 350, date: new Date().toLocaleString()}
+  ];
+
+  return (
+    <MUIList dense={false} className={classes.list} >
+      {transactions.map(transiction => (
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transiction.id} >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar
+                className={ transiction.type === "Income" ? classes.avatarIncome : classes.avatarExpense } >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={transiction.category}
+              secondary={`$${transiction.amount} - ${transiction.date}`}
+            />
+            <ListItemSecondaryAction >
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => {}}
+              >
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  )
+}
+
+export default List;
