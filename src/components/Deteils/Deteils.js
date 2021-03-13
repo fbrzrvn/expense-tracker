d75cd02d4ca798c765d810ce8129c452d0013c6a
fabrizio <faber.rvn@gmail.com>
2021-03-13 17:15:46 +0100

    #15 Custom hook

--- src/components/Deteils/Deteils.js
+++ src/components/Deteils/Deteils.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
-
+import useTrasactions from './useTransactions';
 import useStyles from './styles';
 
 const Deteils = ({ title }) => {
 
+  const { total, chartData } = useTrasactions(title);
   const classes = useStyles();
+
   return (
     <Card className={ title === 'Income' ? classes.income : classes.expense }>
-      <CardHeader />
+      <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">{title}</Typography>
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   )
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -5,6 +5,20 @@ export default makeStyles((theme) => ({
     lineHeight: '1.5rem',
     marginTop: '20px',
   },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
   cartContent: {
     paddingTop: 0,
   },
