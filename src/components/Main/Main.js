a83d0ea2e1306bbe558f0cf4391d3e8f00058751
fabrizio <faber.rvn@gmail.com>
2021-03-21 20:18:24 +0100

    #21 Initial Message

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,34 +1,44 @@
 import React, { useContext } from 'react';
-import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import {
+	Card,
+	CardHeader,
+	CardContent,
+	Typography,
+	Grid,
+	Divider,
+} from '@material-ui/core';
 import Form from './Form/Form';
 import List from './List/List';
 import { ExpenseTrackerContext } from '../../context/context';
 import useStyles from './styles';
 
 const Main = () => {
+	const { balance } = useContext(ExpenseTrackerContext);
+	const classes = useStyles();
 
-  const { balance } = useContext(ExpenseTrackerContext);
-  const classes = useStyles();
-
-  return (
-    <Card className={classes.root}>
-      <CardHeader title="Expense Tracker" />
-      <CardContent>
-        <Typography variant="h5">Total Balance ${balance} </Typography>
-        <Typography variant="subtitle1" className={classes.subtitle}>
-          <Divider className={classes.divider} />
-          <Form />
-        </Typography>
-      </CardContent>
-      <CardContent className={classes.cardContent}>
-        <Grid container spacing={2} >
-          <Grid item xs={12} >
-           <List />
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-  )
-}
+	return (
+		<Card className={classes.root}>
+			<CardHeader title="Expense Tracker" />
+			<CardContent>
+				<Typography variant="h5">Total Balance ${balance} </Typography>
+				{balance === 0 ? (
+					<Typography variant="subtitle1" className={classes.subtitle}>
+						Start with take track of your income and expense just adding them
+						using the form below
+					</Typography>
+				) : null}
+				<Divider className={classes.divider} />
+				<Form />
+			</CardContent>
+			<CardContent className={classes.cardContent}>
+				<Grid container spacing={2}>
+					<Grid item xs={12}>
+						<List />
+					</Grid>
+				</Grid>
+			</CardContent>
+		</Card>
+	);
+};
 
 export default Main;
